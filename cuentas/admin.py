79a60630330d62a7afdb7b6307827c9f6c6449ac
Bueno <pablobuenorodriguez30@gmail.com>
2026-05-17 21:17:54 -0400

    Hol, creacion de la busqueda por partidas arancelarias

--- cuentas/admin.py
+++ cuentas/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+from .models import Arancel
 
-# Register your models here.
+# Definimos cómo se importarán los datos
+class ArancelResource(resources.ModelResource):
+    class Meta:
+        model = Arancel
+        # Le decimos que el 'codigo' es la llave principal (no se repetirá)
+        import_id_fields = ('codigo',) 
+
+# Integramos la carga masiva en el panel
+@admin.register(Arancel)
+class ArancelAdmin(ImportExportModelAdmin):
+    resource_classes = [ArancelResource] # Conectamos la configuración anterior
+    list_display = ('codigo', 'descripcion', 'ga_porcentaje', 'unidad_medida')
+    search_fields = ('codigo', 'descripcion')
