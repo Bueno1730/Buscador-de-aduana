dd6eb0d08f92e2318891ffa4b96472916a17531d
kwyxzel <thegus.serrudo@gmail.com>
2026-05-24 21:26:34 -0400

    modificaciones

--- reportes/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import ReporteError
-
-@admin.register(ReporteError)
-class ReporteErrorAdmin(admin.ModelAdmin):
-    list_display = ['id', 'titulo', 'categoria', 'estado', 'prioridad', 'nombre_reportante', 'fecha_creacion']
-    list_filter = ['estado', 'categoria', 'prioridad']
-    search_fields = ['titulo', 'descripcion', 'email_reportante']
-    list_editable = ['estado', 'prioridad']
